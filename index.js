2405327df6fc5b3545006cdb0a6cc8f1dc29bb05
yago <yagom10@hotmail.com>
2023-04-14 20:49:27 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobbie', 'Sammie', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name) {
+    return drivers.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, letters) {
+    let lenghtOfLetters = letters.length;
+    return drivers.filter(function (driverName) {
+        return driverName.slice(0, lenghtOfLetters) === letters;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
